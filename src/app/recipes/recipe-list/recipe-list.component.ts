d137605be9ffa63db6dc984a767e20244ad86d51
jscodebit <soujanya.janapatla@gmail.com>
2021-10-25 21:55:12 -0400

    Configured recipe and shopping-list components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,12 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes : Recipe[] = [new Recipe('Hot Chocolate Coffee', 'My hot chocolate recipe collection is growing fast and furiously thanks to the awesome Hot Chocolate Extravaganza that I’m running on my blog these days.', 'https://res.cloudinary.com/jscodebit/image/upload/v1528372377/central-coffee/coffee.jpg'),
-  new Recipe('Hot Chocolate Coffee', 'My hot chocolate recipe collection is growing fast and furiously thanks to the awesome Hot Chocolate Extravaganza that I’m running on my blog these days.', 'https://res.cloudinary.com/jscodebit/image/upload/v1528372377/central-coffee/coffee.jpg')];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes : Recipe[] = [
+    new Recipe('Hot Chocolate Coffee', 'My hot chocolate recipe collection is growing fast and furiously thanks to the awesome Hot Chocolate Extravaganza that I’m running on my blog these days.', 'https://res.cloudinary.com/jscodebit/image/upload/v1528372377/central-coffee/coffee.jpg'),
+    new Recipe('Black and White Hot Cocoa', 'Steamed milk and mocha sauce topped with sweetened whipped cream and a chocolate-flavored drizzle. A timeless classic made to sweeten your spirits.', 'https://res.cloudinary.com/jscodebit/image/upload/v1528372377/central-coffee/coffee.jpg')
+  ];
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,9 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { Recipe } from "./recipe.model";
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html'
 })
 export class RecipesComponent{
+  selectedRecipe : Recipe;
+  getSelectedRecipeInfo(recipe: Recipe){
+    this.selectedRecipe = recipe;
+  }
 
 }
